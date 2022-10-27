4f9d2a80e781aad1dd694eabe8d3d8e7bb771aa0
githubofsky <woori21csky@gmail.com>
2022-10-27 11:37:51 +0900

    add cashbook and data

---
+++ script.js
@@ -0,0 +1,25 @@
+//check files
+//let fs = require('fs');
+for (var i = 2209; i < 5000; i++) {
+    var m = String(i);
+    var path = './data/' + m + '/itmes.json';
+    /*
+    if (fs.existSync(path)) {
+        console.log(path);
+    }
+    else {
+        break;
+    }
+    */
+}
+    //var path = './data/'
+
+$.getJSON("./data/2209/itmes.json", function(data) {
+    console.log(data);
+});
+alert('done1');
+
+$.getJSON("./data/2210/itmes.json", function(data) {
+    console.log(data);
+});
+alert('done2');
