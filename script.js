a161b6f483f915134aa86aa77d1e58b67579ca5e
githubofsky <woori21csky@gmail.com>
2022-10-28 12:26:59 +0900

    change graph option

--- script.js
+++ script.js
@@ -14,9 +14,12 @@ for (var i = 2209; i < 5000; i++) {
 */
     //var path = './data/'
 
-var profit_item = [];
+var salary_item = [];
+var bonus_item = [];
+var income_item = [];
 var loss_item = [];
-{
+var items = [];
+
 $.getJSON("./data/2209/items.json", function(data) {
     items = data['items']
     console.log(items);
@@ -27,8 +30,12 @@ $.getJSON("./data/2209/items.json", function(data) {
         var price = Number(items[i]['price'])
         if (price >= 0) {
             profit += price;
+            if (items[i]['label'] == "월급") {
+                salary_item.push(items[i]);
+            }
         } else {
             loss -= price;
+            loss_item.push(items[i])
         }
     }
     sum = profit - loss;
@@ -54,40 +61,34 @@ $.getJSON("./data/2209/items.json", function(data) {
         ctx.fillStyle = 'rgba(255, 99, 132, 1)';
     }
     ctx.fillText(sum.toLocaleString(), w*0.44, 300);
-});
-};
 
-// draw profit title
-{
-    const ctx = document.getElementById('profitTitle').getContext('2d');
-    ctx.canvas.width = window.innerWidth;
-    var w = ctx.canvas.width;
-    ctx.font = "60px '맑은 고딕'";
-    ctx.textAlign = 'center';
-    ctx.textBaseline = 'hanging';
-    ctx.fillText('수입', w*0.5, 0);
-};
-// draw profit chart 
-//Chart.defaults.font.family = '맑은 고딕';
-Chart.defaults.font.size = 50;
-{
-    const ctx = document.getElementById('profitChart').getContext('2d');
-    const myChart = new Chart(ctx, {
+    // draw profit title
+    const ctx_pt = document.getElementById('profitTitle').getContext('2d');
+    ctx_pt.canvas.width = window.innerWidth;
+    var w = ctx_pt.canvas.width;
+    ctx_pt.font = "60px '맑은 고딕'";
+    ctx_pt.textAlign = 'center';
+    ctx_pt.textBaseline = 'hanging';
+    ctx_pt.fillText('수입', w*0.5, 0);
+
+    // draw profit chart 
+    Chart.defaults.font.size = 50;
+    console.log(salary_item);
+    console.log(loss_item.length);
+    var salary_price = 0;
+    for (var i=0; i<salary_item.length; i++) {
+        salary_price += Number(salary_item[i]['price'])
+    }
+    console.log(salary_price);
+    const ctx_pc = document.getElementById('profitChart').getContext('2d');
+    const pChart = new Chart(ctx_pc, {
         type: 'doughnut',
         data: {
             labels: ['월급', '성과급', '기타소득'],
             datasets: [{
-                label: '# of Votes',
-                data: [12, 19, 3, 5, 2, 3],
+                label: 'Amount',
+                data: [salary_price, 2, 3],
                 backgroundColor: [
-                    'rgba(255, 99, 132, 0.2)',
-                    'rgba(54, 162, 235, 0.2)',
-                    'rgba(255, 206, 86, 0.2)',
-                    'rgba(75, 192, 192, 0.2)',
-                    'rgba(153, 102, 255, 0.2)',
-                    'rgba(255, 159, 64, 0.2)'
-                ],
-                borderColor: [
                     'rgba(255, 99, 132, 1)',
                     'rgba(54, 162, 235, 1)',
                     'rgba(255, 206, 86, 1)',
@@ -95,19 +96,13 @@ Chart.defaults.font.size = 50;
                     'rgba(153, 102, 255, 1)',
                     'rgba(255, 159, 64, 1)'
                 ],
-                borderWidth: 1
+                hoverOffset: 4
             }]
-        },
-        options: {
-            scales: {
-                y: {
-                    beginAtZero: true
-                }
-            }
         }
     });
 
-};
+
+});
 {
 const ctx = document.getElementById('myChart').getContext('2d');
 const myChart = new Chart(ctx, {
